c18a5a6690e05b5a3fd6a221f205fe9c29a1ec21
Hung H Ho <kenzensoft@gmail.com>
2018-11-27 18:38:34 -0800

    pull to refresh vm

---
+++ PullToRefreshTest/PullToRefreshTest/ViewModels/ABaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PullToRefreshTest.Properties;
+
+namespace PullToRefreshTest.ViewModels
+{
+    public abstract class ABaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PullToRefreshTest/PullToRefreshTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PullToRefreshTest.Properties;
+
+namespace PullToRefreshTest.ViewModels
+{
+    public class MainViewModel : ABaseViewModel
+    {
+    }
+}
